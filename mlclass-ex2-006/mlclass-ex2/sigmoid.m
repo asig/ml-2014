77cfd61213272e097be2b733136f7ff872270eee
Andreas <andreas@fractal-labs.com>
2014-07-18 16:07:23 +0200

    submitted solution

--- mlclass-ex2-006/mlclass-ex2/sigmoid.m
+++ mlclass-ex2-006/mlclass-ex2/sigmoid.m
@@ -9,8 +9,14 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+rows = size(z)(1);
+cols = size(z)(2);
 
-
+for i = 1:rows
+	for j = 1:cols
+        g(i,j) = 1/(1+exp(-z(i,j)));
+    end
+end
 
 
 % =============================================================
